f4b82f1c44622ee993a75eb2247899004229d6b1
Barrett Williams <barrett@tutonic.com>
2012-03-26 12:35:32 -0400

    Rudimentary set-up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutonicOdeskTest::Application.config.secret_token = '8ea3f0706490a3e588954586b9f0bf81ebb28b1ff7082aa32e94c8bd1ca25bfd3725e08f4467908c3df97e500b64dbc99b09c53fb850fc6858a48575f200b46b'
