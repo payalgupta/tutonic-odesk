295c905b440c6c50527b59b1003a760eccf8e5d3
Barrett Williams <barrett@tutonic.com>
2012-03-27 21:58:27 -0400

    Pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def privacy
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 TutonicOdeskTest::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/help"
+
+  get "pages/about"
+
+  get "pages/privacy"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
